af4a85580a85e587a68c4e91ac4af8a437ded7d1
CrisisPena1 <99937088+CrisisPena1@users.noreply.github.com>
2022-03-19 12:50:34 -0700

    Update app.js

--- source/app.js
+++ source/app.js
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(misRutas);
 
 app.listen(puerto, ()=>{
-    console.log("iniciamos el puerto");
+    console.log("iniciamos el puerto 8080");
 
 })
